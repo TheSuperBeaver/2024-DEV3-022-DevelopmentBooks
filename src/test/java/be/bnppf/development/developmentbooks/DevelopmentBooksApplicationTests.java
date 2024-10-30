4f32f5e3f7a3a4445ef8ae2b61f447b1e5d1bf4b
TheSuperBeaver <mestrez.matthieu@gmail.com>
2024-10-30 13:47:55 +0100

    Initial Commit - Spring Initializer

---
+++ src/test/java/be/bnppf/development/developmentbooks/DevelopmentBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package be.bnppf.development.developmentbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
